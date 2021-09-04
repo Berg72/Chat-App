88b9a320a1d09069e71c6922cdb0d9c072dd2570
mark bergeson <markbergeson@Marks-MacBook-Pro.local>
2021-09-04 15:50:08 -0600

    creating chat-app

---
+++ Chat App/Chat App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chat App
+//
+//  Created by Mark bergeson on 9/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
